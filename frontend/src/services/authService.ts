b4c9b19f18aa717a9ef63de25cb1beb5f6a983a3
Ayush Anand <ayushanand0208@gmail.com>
2025-04-15 13:45:38 +0530

    changed frontend

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -4,35 +4,35 @@ const API_URL = 'http://localhost:5000/api/auth/';
 
 // Register user
 const register = async (userData: { username: string; password: string }) => {
-    const response = await axios.post(API_URL + 'register', userData);
+  const response = await axios.post(API_URL + 'register', userData);
 
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data.data));
-    }
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data.data));
+  }
 
-    return response.data.data;
+  return response.data.data;
 };
 
 // Login user
 const login = async (userData: { username: string; password: string }) => {
-    const response = await axios.post(API_URL + 'login', userData);
+  const response = await axios.post(API_URL + 'login', userData);
 
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data.data));
-    }
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data.data));
+  }
 
-    return response.data.data;
+  return response.data.data;
 };
 
 // Logout user
 const logout = () => {
-    localStorage.removeItem('user');
+  localStorage.removeItem('user');
 };
 
 const authService = {
-    register,
-    login,
-    logout,
+  register,
+  login,
+  logout,
 };
 
 export default authService;
