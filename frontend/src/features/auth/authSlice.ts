b89a4f2ddf66c11d3225059589ea8b50cb9095c3
Ayush Anand <ayushanand0208@gmail.com>
2025-04-15 13:46:20 +0530

    changed frontend

--- frontend/src/features/auth/authSlice.ts
+++ frontend/src/features/auth/authSlice.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 import authService from '../../services/authService';
 
